361bdf88167029db10a1a3f56daeb39ff165f016
amyhenke <amyleehenke1@gmail.com>
2025-01-12 17:34:07 +0000

    add product card and format price

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,16 @@
-import { Button } from "@/components/ui/button";
+import sampleData from "@/db/sample-data";
+import ProductList from "@/components/shared/product/product-list";
 
 const Homepage = () => {
-    return <Button>Button</Button>;
+    return (
+        <>
+            <ProductList
+                data={sampleData.products}
+                title="Newest Arrivals"
+                limit="4"
+            />
+        </>
+    );
 };
 
 export default Homepage;
