f6916d6801db523241b5b775be01e98441d4c55d
amyhenke <amyleehenke1@gmail.com>
2025-01-13 18:22:50 +0000

    add product details page

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,13 +1,11 @@
 "use server"
 
-import { PrismaClient } from "@prisma/client"
+import { prisma } from "@/db/prisma"
 import { convertToPlainObject } from "../utils"
 import { LATEST_PRODUCTS_LIMIT } from "../constants"
 
 // Get latest products
 export async function getLatestProducts() {
-    const prisma = new PrismaClient()
-
     const data = await prisma.product.findMany({
         // limit to 4
         take: LATEST_PRODUCTS_LIMIT,
@@ -16,4 +14,11 @@ export async function getLatestProducts() {
     })
 
     return convertToPlainObject(data)
+}
+
+// Get single product by slug
+export async function getProductBySlug(slug: string) {
+    return await prisma.product.findFirst({
+        where: { slug: slug }
+    })
 }
